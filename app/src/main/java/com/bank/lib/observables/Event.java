981b63346845c3f3efeb2b44c9b2f2176cf914cf
Andrés Téllez <andres.tellez@sofka.com.co>
2024-12-30 21:39:47 -0500

    lib: add events and subscribers

---
+++ app/src/main/java/com/bank/lib/observables/Event.java
@@ -0,0 +1,38 @@
+package com.bank.lib.observables;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.List;
+
+import com.bank.lib.subscribers.Subscriber;
+
+public class Event<T> {
+    private T payload;
+
+    public static class Listeners {
+        private Map<Class<Event<?>>, List<Subscriber<Event<?>>>> listeners;
+
+        public Listeners() {
+            this.listeners = new HashMap<>();
+        }
+
+        public void add(Subscriber<Event<?>> subscriber) {
+            this.listeners.computeIfAbsent(subscriber.event(), k -> new LinkedList<>())
+                    .add(subscriber);
+        }
+
+        public <T extends Event<?>> void dispatch(T event) {
+            forEvent(event.getClass())
+                    .forEach(listener -> listener.update(event));
+        }
+
+        private List<Subscriber<Event<?>>> forEvent(Class<?> event) {
+            return this.listeners.get(event);
+        }
+    }
+
+    public T getPayload() {
+        return this.payload;
+    }
+}
